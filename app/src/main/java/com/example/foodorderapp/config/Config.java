e8c3447b202f56906d89b0b338a6a4e4e06e4ac5
khuong.hm <khuong.hm@tinasoft.com.vn>
2025-05-06 01:18:23 +0700

    Connect api verify otp

--- app/src/main/java/com/example/foodorderapp/config/Config.java
+++ app/src/main/java/com/example/foodorderapp/config/Config.java
@@ -1,5 +1,6 @@
 package com.example.foodorderapp.config;
 
 public class Config {
+//    public static String BE_URL = "http://172.11.253.71:3001/api/v1";
     public static String BE_URL = "http://192.168.60.103:3001/api/v1";
 }
