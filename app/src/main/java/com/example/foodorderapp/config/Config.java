02ded2d659168f11c76bfbd30781fe70628d7855
khuong.hm <khuong.hm@tinasoft.com.vn>
2025-05-06 01:29:21 +0700

    Connect api reset password

--- app/src/main/java/com/example/foodorderapp/config/Config.java
+++ app/src/main/java/com/example/foodorderapp/config/Config.java
@@ -2,5 +2,6 @@ package com.example.foodorderapp.config;
 
 public class Config {
 //    public static String BE_URL = "http://172.11.253.71:3001/api/v1";
-    public static String BE_URL = "http://192.168.60.103:3001/api/v1";
+//    public static String BE_URL = "http://192.168.60.103:3001/api/v1";
+    public static String BE_URL = "http://192.168.2.100:3001/api/v1";
 }
